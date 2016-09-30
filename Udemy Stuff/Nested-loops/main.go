a27a964710478e5948c45a695900a63f974eaec5
Gentry Rolofson <xdarkicex@gmail.com>
2016-09-29 21:33:08 -0700

    pam anderson is going to love me.

--- Udemy Stuff/Nested-loops/main.go
+++ Udemy Stuff/Nested-loops/main.go
@@ -3,46 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	nested()
+	//nested()
+	var v bool
+	Nests(v)
 }
 
-func nested() {
+//Nests demonstrates nested funct
+func Nests(v bool) bool {
 
-	for {
-		for i := 0; i < 2; i++ {
-			fmt.Printf("%s%d\n", "Loop 1: ", i)
-			for i := 0; i < 3; i++ {
-				fmt.Printf("%s%d\n", "Loop 2: ", i)
-				for i := 0; i < 4; i++ {
-					fmt.Printf("%s%d\n", "Loop 3: ", i)
-					for i := 0; i < 5; i++ {
-						fmt.Printf("%s%d\n", "Loop 4: ", i)
-						for i := 0; i < 6; i++ {
-							fmt.Printf("%s%d\n", "Loop 5: ", i)
-							for i := 0; i < 7; i++ {
-								fmt.Printf("%s%d\n", "Loop 6: ", i)
-								for i := 0; i < 8; i++ {
-									fmt.Printf("%s%d\n", "Loop 7: ", i)
-									for i := 0; i < 9; i++ {
-										fmt.Printf("%s%d\n", "Loop 8: ", i)
-										for i := 0; i < 10; i++ {
-											fmt.Printf("%s%d\n", "Loop 9: ", i)
-											for i := 0; i < 11; i++ {
-												fmt.Printf("%s%d\n", "Loop 10: ", i)
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-					break
-				}
+	for true {
+		for i := 0; i < 30; i++ {
+			if i == 20 {
+				fmt.Println("I am Done")
 				break
 			}
-			break
+			fmt.Println(i)
+
 		}
-		fmt.Println("I am done")
-		break
+		return false
 	}
+
+	return false
 }
